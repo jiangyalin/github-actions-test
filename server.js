1b41b3f2ead271aa85d4584bee7c1ffec55abbcb
yalin.jiang <yalin.jiang@71360.com>
2019-09-26 15:31:31 +0800

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const expressStaticGzip = require('express-static-gzip')
+const history = require('connect-history-api-fallback')
+
+app.use('/', expressStaticGzip('dist'))
+
+app.use(history({
+  verbose: true,
+  index: '/'
+}))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
+
+app.listen(80, () => {
+  console.log('服务启动' + 80)
+})
